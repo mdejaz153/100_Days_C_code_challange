f1678b2ddfe51833e65295def2a4c61c49979e19
mdejaz153 <mdejazms365@gmail.com>
2025-12-01 00:25:39 +0530

    Update q109.c

--- q109.c
+++ q109.c
@@ -19,3 +19,38 @@
 
 // */
 
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+    if (k > n) {
+        printf("Invalid\n");
+        return 0;
+    }
+
+    int window_sum = 0;
+    for (int i = 0; i < k; i++) {
+        window_sum += arr[i];
+    }
+
+    int max_sum = window_sum;
+
+    for (int i = k; i < n; i++) {
+        window_sum = window_sum + arr[i] - arr[i - k];
+        if (window_sum > max_sum)
+            max_sum = window_sum;
+    }
+
+    printf("%d\n", max_sum);
+
+    return 0;
+}
