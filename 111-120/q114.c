01316b299007ca93e6bffdbc9f928fd98e488fbe
mdejaz153 <mdejazms365@gmail.com>
2025-12-01 01:27:09 +0530

    Update q114.c

--- 111-120/q114.c
+++ 111-120/q114.c
@@ -18,3 +18,33 @@
 // 3
 
 // */
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    scanf("%s", s);
+
+    int n = strlen(s);
+    int maxLen = 0;
+
+    for (int i = 0; i < n; i++) {
+        int freq[256] = {0};
+        int currLen = 0;
+
+        for (int j = i; j < n; j++) {
+            if (freq[(unsigned char)s[j]] == 1)
+                break;
+
+            freq[(unsigned char)s[j]] = 1;
+            currLen++;
+        }
+
+        if (currLen > maxLen)
+            maxLen = currLen;
+    }
+
+    printf("%d", maxLen);
+    return 0;
+}
