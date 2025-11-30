c97ceb9666881ac83848d8fcd55cc42d63c3e807
mdejaz153 <mdejazms365@gmail.com>
2025-12-01 01:26:37 +0530

    Update q112.c

--- 111-120/q112.c
+++ 111-120/q112.c
@@ -18,3 +18,30 @@
 // 25
 
 // */
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int maxSoFar = arr[0];
+    int currentSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (currentSum + arr[i] < arr[i])
+            currentSum = arr[i];
+        else
+            currentSum += arr[i];
+
+        if (currentSum > maxSoFar)
+            maxSoFar = currentSum;
+    }
+
+    printf("%d", maxSoFar);
+    return 0;
+}
