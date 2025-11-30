14b3925301c6a7c68bcdf7947743b89f413ae329
mdejaz153 <mdejazms365@gmail.com>
2025-12-01 01:25:23 +0530

    Update q109.c

--- 101-110/q109.c
+++ 101-110/q109.c
@@ -19,3 +19,31 @@
 
 // */
 
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &k);
+
+    int maxSum = 0;
+
+    for (int i = 0; i <= n - k; i++) {
+        int currentSum = 0;
+
+        for (int j = i; j < i + k; j++) {
+            currentSum += arr[j];
+        }
+
+        if (currentSum > maxSum)
+            maxSum = currentSum;
+    }
+
+    printf("%d", maxSum);
+    return 0;
+}
