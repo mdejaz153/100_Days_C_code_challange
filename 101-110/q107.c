bf7c760c5cbcc538c7b15d3cdac26175e8494551
mdejaz153 <mdejazms365@gmail.com>
2025-12-01 01:24:51 +0530

    Update q107.c

--- 101-110/q107.c
+++ 101-110/q107.c
@@ -32,3 +32,32 @@
 // -1, 5, 4, 3
 
 // */
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        int prevGreater = -1;
+
+        for (int j = i - 1; j >= 0; j--) {
+            if (arr[j] > arr[i]) {
+                prevGreater = arr[j];
+                break;
+            }
+        }
+
+        if (i == n - 1)
+            printf("%d", prevGreater);
+        else
+            printf("%d, ", prevGreater);
+    }
+
+    return 0;
+}
