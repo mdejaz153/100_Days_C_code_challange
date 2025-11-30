75ef12ecfc2125f5b96115b69eda0620c27bd579
mdejaz153 <mdejazms365@gmail.com>
2025-12-01 01:24:13 +0530

    Update q106.c

--- 101-110/q106.c
+++ 101-110/q106.c
@@ -1,4 +1,4 @@
-// Q106: Write a program to take an array arr[] of integers as input, 
+//Q106: Write a program to take an array arr[] of integers as input, 
 //the task is to find the next greater element for each element of the array 
 //in order of their appearance in the array. 
 //Next greater element of an element in the array is the 
@@ -10,7 +10,7 @@
 // - Print the output for each element in a comma separated fashion.
 // - Do not use Stack, use brute force approach (nested loop) to solve.
 
-// /*
+// /*//
 // Sample Test Cases:
 // Input 1:
 // arr = [1, 3, 2, 4]
@@ -34,3 +34,31 @@
 
 // */
 
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        int nextGreater = -1;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+                nextGreater = arr[j];
+                break;
+            }
+        }
+
+        if (i == n - 1)
+            printf("%d", nextGreater);
+        else
+            printf("%d, ", nextGreater);
+    }
+
+    return 0;
+}
